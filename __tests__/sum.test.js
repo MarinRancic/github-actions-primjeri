abf2fd7de48b14f196e191d84b834e45b3ff8a37
MarinRancic <marin.rancic1@gmail.com>
2025-12-16 22:53:33 +0100

    fail test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 5 + 7 to equal 12', () => {
-  expect(sum(5, 7)).toBe(12);
+test('adds 5 + 7 to equal 13', () => {
+  expect(sum(5, 7)).toBe(13);
 });
